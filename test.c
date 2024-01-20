774fee45d0518a4b1b432ec947e3347f8aafe8ba
owrmille <iasemin.topchu@gmail.com>
2024-01-20 16:18:53 +0100

    Delete libft (it's empty)

--- test.c
+++ test.c
@@ -1,9 +1,11 @@
 #include "ft_printf.h"
 # include <stdio.h>
+
 // to compile and run:
 // 1. make
 // 2. cc -Wall -Wextra -Werror -o test test.c -L. -lftprintf
 // (where test.c can be any file with tests)
+
 int test_c()
 {
 	char	*content = "My grade is %c|";
